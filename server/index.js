9b925dbf1bc0bbbada066b7a95cb78cec45e3b3c
Book Pauk <bookpauk@gmail.com>
2018-12-26 21:06:22 +0700

    Корневой файл сервера

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res) {
-    res.send('hello world');
-});
+const config = require('./config/config');
+const logger = require('./core/loggerInit')(config);
 
-app.listen(3000, "0.0.0.0", function() {
-    console.log("server started");
-});
+const SqliteConnectionPool = require('./core/SqliteConnectionPool');
+
+async function main() {
+    const connPool = new SqliteConnectionPool(20, logger, config);
+    await connPool.init();
+
+    app.use(app.json());
+    app.static('public');
+
+    require('./routes')(app, connPool, logger, config);
+
+    app.listen(config.port, config.ip, function() {
+        logger.log('Server is ready');
+    });
+}
+
+main();
