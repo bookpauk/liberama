9e00e7356ff9f4fdfde727e03b0365e6881994b1
Book Pauk <bookpauk@gmail.com>
2019-01-01 23:08:13 +0700

    Мелкий рефакторинг

--- client/api/misc.js
+++ client/api/misc.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
+const api = axios.create({
+  baseURL: '/api'
+});
+
 class Misc {
     async loadConfig() {
-        const response = await axios.post('/api/config', {params: ['name', 'version']});
+        const response = await api.post('/config', {params: ['name', 'version']});
         return response.data;
     }
 }
 
-let misc = null;
-if (!misc) {
-    misc = new Misc();
-}
-
-export default misc;
+export default new Misc();
