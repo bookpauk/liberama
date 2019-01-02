86e4c8013a729fefed4b636f5fecf792dc00a4d0
Book Pauk <bookpauk@gmail.com>
2019-01-02 15:32:40 +0700

    Включил русский язык

--- client/main.js
+++ client/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue';
 import App from './components/App.vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/ru-RU';
 import store from './store';
 
 //Vue.config.productionTip = false;
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 
 new Vue({
     store,
