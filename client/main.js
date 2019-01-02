61eea9ccccab10143eec25be839c7e7b4a74de2a
Book Pauk <bookpauk@gmail.com>
2019-01-02 16:12:05 +0700

    Кастомная тема

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './components/App.vue';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import './theme/index.css';
 import locale from 'element-ui/lib/locale/lang/ru-RU';
 import store from './store';
 
