eda77248d91f8364887f9c4ecb046bcd8a13bf71
Book Pauk <bookpauk@gmail.com>
2019-01-03 20:47:14 +0700

    Часть компонентов загружаем динамически

--- client/router.js
+++ client/router.js
@@ -11,12 +11,12 @@ import Book from  './components/CardIndex/Book/Book.vue';
 import History from  './components/CardIndex/History/History.vue';
 
 import Reader from './components/Reader/Reader.vue';
-//import Forum from './components/Forum/Forum.vue';
-import Income from './components/Income/Income.vue';
-import Sources from './components/Sources/Sources.vue';
-import Settings from './components/Settings/Settings.vue';
-import Help from './components/Help/Help.vue';
-import NotFound404 from './components/NotFound404/NotFound404.vue';
+//const Forum = () => import('./components/Forum/Forum.vue');
+const Income = () => import('./components/Income/Income.vue');
+const Sources = () => import('./components/Sources/Sources.vue');
+const Settings = () => import('./components/Settings/Settings.vue');
+const Help = () => import('./components/Help/Help.vue');
+const NotFound404 = () => import('./components/NotFound404/NotFound404.vue');
 
 const myRoutes = [
     ['/', null, null, '/cardindex'],
