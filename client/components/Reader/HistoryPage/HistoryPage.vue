724cc750fea779556a5de11faed4af398a1bf609
Book Pauk <bookpauk@gmail.com>
2019-01-25 23:00:39 +0700

    Работа над HistoryPage

--- client/components/Reader/HistoryPage/HistoryPage.vue
+++ client/components/Reader/HistoryPage/HistoryPage.vue
@@ -1,7 +1,14 @@
 <template>
-    <el-container>
-        Раздел History в разработке
-    </el-container>
+    <div ref="main" class="main">
+        <div class="window">
+            <div class="header">
+                <span class="header-text">Прочитаные книги:</span>
+            </div>
+            <div class="list">
+                Test
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -13,8 +20,48 @@ export default @Component({
 })
 class HistoryPage extends Vue {
     created() {
+        this.commit = this.$store.commit;
     }
 
 }
 //-----------------------------------------------------------------------------
 </script>
+
+<style scoped>
+.main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.window {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    min-width: 200px;
+    max-width: 600px;
+    background-color: #f5f7fa;
+    margin: 10px;
+    border: 1px solid black;
+    box-shadow: 3px 3px 5px black;
+}
+
+.header {
+    display: flex;
+    align-items: center;
+    height: 40px;
+}
+
+.header-text {
+    margin-left: 10px;
+}
+
+.list {
+    flex: 1;
+    background-color: #ffffff;
+}
+
+.list-item {
+}
+</style>
