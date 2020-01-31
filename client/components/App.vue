5ddf19be4d27eedbec953b83290ca08733f44dbf
Book Pauk <bookpauk@gmail.com>
2020-01-31 21:14:09 +0700

    Удалил мусор

--- client/components/App.vue
+++ client/components/App.vue
@@ -284,14 +284,6 @@ body, html, #app {
     font: normal 12pt ReaderDefault;
 }
 
-.el-tabs__content {
-    flex: 1;
-    padding: 0 !important;
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
-}
-
 @font-face {
   font-family: 'ReaderDefault';
   src: url('fonts/reader-default.woff') format('woff'),
