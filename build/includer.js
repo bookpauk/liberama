13efd50d8042a3ddae60851dab8c9c87fdd9c794
Book Pauk <bookpauk@gmail.com>
2020-02-09 16:43:36 +0700

    Добавлен универсальный includer для включения файлов в исходник

---
+++ build/includer.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+exports.default = function includer(source) {
+    const lines = source.split('\n');
+
+    let result = [];
+    for (const line of lines) {
+        const trimmed = line.trim();
+        const m = trimmed.match(/^@@[\s]*?include[\s]*?\(['"](.*)['"]\)/);
+        if (m) {
+            const includedFile = path.resolve(path.dirname(this.resourcePath), m[1]);
+            const fileContent = fs.readFileSync(includedFile, 'utf8');
+            result.push(fileContent);
+        } else {
+            result.push(line);
+        }
+    }
+    return result.join('\n');
+}
