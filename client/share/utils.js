a0242953792cd8892f5b2899a946e7d1613af5ee
Book Pauk <bookpauk@gmail.com>
2020-03-15 21:48:53 +0700

    Мелкий рефакторинг

--- client/share/utils.js
+++ client/share/utils.js
@@ -206,7 +206,9 @@ export function escapeXml(str) {
 
 export function keyEventToCode(event) {
     let result = [];
-    const modCode = event.code.substring(0, 3);
+    let code = event.code;
+
+    const modCode = code.substring(0, 3);
     if (event.metaKey && modCode != 'Met')
         result.push('Meta');
     if (event.ctrlKey && modCode != 'Con')
@@ -216,7 +218,6 @@ export function keyEventToCode(event) {
     if (event.altKey && modCode != 'Alt')
         result.push('Alt');
     
-    let code = event.code;
     if (modCode == 'Dig') {
         code = code.substring(5, 6);
     } else if (modCode == 'Key') {
