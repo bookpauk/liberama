ebe42956ada40f04cbd3b7ece372f758ccfe3173
Book Pauk <bookpauk@gmail.com>
2020-10-28 18:27:24 +0700

    Все компоненты загружаются динамически

--- client/router.js
+++ client/router.js
@@ -2,18 +2,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import _ from 'lodash';
 
-//немедленная загрузка
-import CardIndex from './components/CardIndex/CardIndex.vue';
-//const CardIndex = () => import('./components/CardIndex/CardIndex.vue');
-
+const CardIndex = () => import('./components/CardIndex/CardIndex.vue');
 const Search = () => import('./components/CardIndex/Search/Search.vue');
 const Card = () => import('./components/CardIndex/Card/Card.vue');
 const Book = () => import('./components/CardIndex/Book/Book.vue');
 const History = () => import('./components/CardIndex/History/History.vue');
-
+const Reader = () => import('./components/Reader/Reader.vue');
 //немедленная загрузка
-//const Reader = () => import('./components/Reader/Reader.vue');
-import Reader from './components/Reader/Reader.vue';
+//import Reader from './components/Reader/Reader.vue';
 
 //const Forum = () => import('./components/Forum/Forum.vue');
 const Income = () => import('./components/Income/Income.vue');
