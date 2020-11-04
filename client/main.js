d540cb91a95be16eb6c97f3195d67979312ef607
Book Pauk <bookpauk@gmail.com>
2020-11-04 14:41:11 +0700

    Добавлен пакет vue-sanitize

--- client/main.js
+++ client/main.js
@@ -4,6 +4,9 @@ import router from './router';
 import store from './store';
 import './quasar';
 
+import vueSanitize from 'vue-sanitize';
+Vue.use(vueSanitize);
+
 import App from './components/App.vue';
 //Vue.config.productionTip = false;
 Vue.prototype.$isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
