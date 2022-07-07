c6518365548254e5fe51044570cad599a644d2b8
Book Pauk <bookpauk@gmail.com>
2022-07-07 19:33:32 +0700

    Поправки скриптов запуска

--- docs/omnireader.ru/cron_server.sh
+++ docs/omnireader.ru/cron_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! pgrep -x "liberama" > /dev/null ; then
-    sudo -H -u www-data bash -c "cd /var/www; /home/liberama/liberama"
+    sudo -H -u www-data bash -c "cd /var/www; /home/liberama/liberama &>/dev/null"
 else
     echo "Process 'liberama' already running"
 fi
--- docs/omnireader.ru/start_server.sh
+++ docs/omnireader.ru/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo -H -u www-data bash -c "cd /var/www; /home/liberama/liberama" & disown
+sudo -H -u www-data bash -c "cd /var/www; /home/liberama/liberama &>/dev/null & disown"
 sudo service cron start
