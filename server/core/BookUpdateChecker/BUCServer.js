5dead039f595071f3cb73c84b701a80d72428f51
Book Pauk <bookpauk@gmail.com>
2022-08-05 01:09:47 +0700

    Дебаг

--- server/core/BookUpdateChecker/BUCServer.js
+++ server/core/BookUpdateChecker/BUCServer.js
@@ -259,8 +259,9 @@ class BUCServer {
                     try {
                         let unchanged = true;
                         let hash = '';
-
+log(`head ${row.id}`)
                         const headers = await this.down.head(row.id);
+log(`headers ${row.id}: ${JSON.stringify(headers)}`)
 
                         const etag = headers['etag'] || '';
                         const modTime = headers['last-modified'] || '';
@@ -273,7 +274,9 @@ class BUCServer {
                             && (!modTime || !row.modTime || (modTime !== row.modTime))
                             && (!size || !row.size || (size !== row.size))
                             ) {
+log(`down ${row.id}`)
                             const downdata = await this.down.load(row.id);
+log(`loaded ${row.id}`)
 
                             size = downdata.length;
                             hash = await utils.getBufHash(downdata, 'sha256', 'hex');
