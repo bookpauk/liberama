e69e9335f908b650fb377ff8921271c68fba6c7b
Book Pauk <bookpauk@gmail.com>
2022-09-04 21:19:30 +0700

    Исправлен баг с формой для доната, показывалась каждый день, а не каждый месяц

--- client/share/utils.js
+++ client/share/utils.js
@@ -46,7 +46,7 @@ export function formatDate(d, format) {
         case 'coDate':
             return `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${d.getDate().toString().padStart(2, '0')}`;
         case 'coMonth':
-            return `${(d.getMonth() + 1).toString().padStart(2, '0')}-${d.getDate().toString().padStart(2, '0')}`;
+            return `${(d.getMonth() + 1).toString().padStart(2, '0')}`;
         case 'noDate':
             return `${d.getDate().toString().padStart(2, '0')}.${(d.getMonth() + 1).toString().padStart(2, '0')}.${d.getFullYear()}`;
     }
