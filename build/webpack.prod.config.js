b58d3a1b8bf4c354c480c87912828faa992eedca
Book Pauk <bookpauk@gmail.com>
2022-09-20 20:21:41 +0700

    Поправки параметров CopyWebpackPlugin

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -54,7 +54,7 @@ module.exports = merge(baseWpConfig, {
             filename: `${publicDir}/index.html`
         }),
         new CopyWebpackPlugin({patterns: 
-            [{from: `${clientDir}/assets/*`, to: `${publicDir}/`, context: `${clientDir}/assets` }]
+            [{context: `${clientDir}/assets`, from: `${clientDir}/assets/*`, to: `${publicDir}/` }]
         }),
         new GenerateSW({
             cacheId: 'liberama',
