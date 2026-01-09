eb6e2f62f9bfabd1c996e2e0e5087e0f3efc49b0
Book Pauk <bookpauk@gmail.com>
2026-01-09 21:23:42 +0700

    К предыдущему

---
+++ client/assets/sw-reg.js
@@ -0,0 +1,16 @@
+(async() => {
+    if('serviceWorker' in navigator) {
+        try {
+            const registrations = await navigator.serviceWorker.getRegistrations();
+
+            for (const registration of registrations) {
+                if (!registration.active.scriptURL.endsWith(`sw.js`))
+                    await registration.unregister();
+            }
+        } catch (e) {
+            console.error(`Deregistration failed with ${e}`);
+        }
+
+        navigator.serviceWorker.register(`./sw.js`);
+    }
+})();
